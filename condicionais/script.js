03b236acade36f51679341271abfb5aa048df0d2
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-01-29 20:13:51 -0300

    Condicionais

---
+++ condicionais/script.js
@@ -0,0 +1,13 @@
+// Condicionais
+let n1 = 10;
+let n2 = 20;
+
+document.write(n1 < n2); // <- true
+
+let media = 7;
+
+if (media >= 7 && media <= 10) {
+    document.write('Aprovado!');
+} else {
+    document.write('Reprovado!');
+}
