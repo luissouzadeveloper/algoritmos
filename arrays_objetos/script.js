0d3e0714a3fb6fd6504cd520a4cbf7c7dad66512
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-01-30 20:01:25 -0300

    Arrays e objetos

---
+++ arrays_objetos/script.js
@@ -0,0 +1,25 @@
+// arrays
+let fruteira = ['Banana', 'Pêra', 'Uva'];
+
+document.write(fruteira[1] + '<br>');
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+document.write(numeros + '<br>');
+
+// objeto
+let pessoa = {
+    nome: 'Maria',
+    idade: 60,
+    programadora: false
+};
+
+document.write(pessoa.programadora + '<br>');
+
+let carro = {
+    modelo: 'HRV',
+    cor: 'Prata',
+    ano: '2019',
+    arCondicionado: true
+};
+document.write(carro.modelo);
